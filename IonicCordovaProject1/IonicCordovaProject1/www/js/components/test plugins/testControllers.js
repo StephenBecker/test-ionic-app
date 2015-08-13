4c3074def2e6088253b0d63fbd181e8747a606c5
Stephen Becker <sbecker@catalystitservices.com>
2015-08-13 11:17:24 -0400

    Adds units to geoloc display

--- IonicCordovaProject1/IonicCordovaProject1/www/js/components/test plugins/testControllers.js
+++ IonicCordovaProject1/IonicCordovaProject1/www/js/components/test plugins/testControllers.js
@@ -7,7 +7,7 @@
 
        
        
-        //  $scope.isOnline = Cordova.isOnline();
+        //  Network Functions;
 
 
         $scope.network = {
@@ -15,6 +15,11 @@
             "Is Online": $cordovaNetwork.isOnline(),
             "Is Offline": $cordovaNetwork.isOffline()
         };
+
+        //
+        //Device info
+        //
+
         $scope.device = {
             // "Device": $cordovaDevice.getDevice(),
             "Cordova Version": $cordovaDevice.getCordova(),
@@ -26,21 +31,26 @@
             "Device Manufacturer": $cordovaDevice.getManufacturer()
         };
 
+
+
+        //
+        //Navigation functions
+        //
         var posOptions = {timeout: 10000, enableHighAccuracy: false};
         $cordovaGeolocation
         .getCurrentPosition(posOptions)
-        .then(function(position) {
-            var lat = position.coords.latitude,
-            long = position.coords.longitude,
-            acc = position.coords.accuracy;
-
-            $scope.navigation = {"Lattitued":lat, "Longitude":long, "Accuracy":acc};
-            });
+        .then(function (position) { updateLoc(position)});
 
 
        
         
-       
+        function updateLoc(position) {
+            var lat = position.coords.latitude,
+            long = position.coords.longitude,
+            acc = position.coords.accuracy;
+
+            $scope.navigation = { "Lattitued": lat, "Longitude": long, "Accuracy": acc + " meters (or " +Math.round(acc*3.28084)+" feet)" };
+        }
        
        
     })
